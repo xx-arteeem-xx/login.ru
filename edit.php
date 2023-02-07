7d370c19dd0108fd11e2e8d1ebfd5c122a83624f
xx-arteeem-xx <artem.firsov.qwe@gmail.com>
2023-02-07 10:25:33 +0700

    fixing profile editing

--- edit.php
+++ edit.php
@@ -4,17 +4,26 @@
 
     $data = $_POST;
     if (isset($data['edit'])){
-        
+
+        $id = $_SESSION['logged_user']->id;
+
+
         if ($data['editname'] != $_SESSION['logged_user']->name){
+            $sql = "UPDATE users SET name = '".$data['editname']."' WHERE id = '".$id."'";
+            R::exec($sql);
             $_SESSION['logged_user']->name = $data['editname'];
         }
         if ($data['editsurname'] != $_SESSION['logged_user']->surname){
+            $sql = "UPDATE users SET surname = '".$data['editsurname']."' WHERE id = '".$id."'";
+            R::exec($sql);
             $_SESSION['logged_user']->surname = $data['editsurname'];
         }
         if ($data['editdescription'] != $_SESSION['logged_user']->description){
+            $sql = "UPDATE users SET description = '".$data['editdescription']."' WHERE id = '".$id."'";
+            R::exec($sql);
             $_SESSION['logged_user']->description = $data['editdescription'];
         }
-
+        
         header('Location: /lk.php');
 
     };
@@ -43,30 +52,30 @@
                 <h3>
                     Имя: 
                 </h3>
-                <input tabindex="2" type="text" name="editname" placeholder="Имя" value="<?php echo $_SESSION['logged_user']->name ?>">
+                <input tabindex="1" type="text" name="editname" required placeholder="Имя" value="<?php echo $_SESSION['logged_user']->name ?>">
             </div>
 
             <div class="name">
                 <h3>
                     Фамилия: 
                 </h3>
-                <input tabindex="3" type="text" name="editsurname" placeholder="Фамилия" value="<?php echo $_SESSION['logged_user']->surname ?>">
+                <input tabindex="2" type="text" name="editsurname" required placeholder="Фамилия" value="<?php echo $_SESSION['logged_user']->surname ?>">
             </div>
 
             <div class="name">
                 <h3>
                     О себе: 
                 </h3>
-                <textarea tabindex="7" name="editdescription"></textarea>
+                <textarea tabindex="3" name="editdescription" value="<?php echo $_SESSION['logged_user']->descpiption ?>"></textarea>
             </div>
 
             
             <div class="name">
-                <button tabindex="8" type="submit" name="edit" >Изменить</button>
+                <button tabindex="4" type="submit" name="edit" >Изменить</button>
             </div>
         </form>
         
-        <a href="img_add.php" style="margin-left: 0"></a>
+        <a href="img_add.php" style="margin-left: 0">Изменить фото профиля</a>
 
     </nav>
 
